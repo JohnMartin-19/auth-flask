6890b8c0a83e461ab86e81101cbe9a07fddf1525
John <johnnkonge2020@gmail.com>
2024-01-17 12:51:20 +0300

    added routes for endpoints

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,Resource,request,response,jsonify,session
+from flask import Flask,Resource,request,response,jsonify,session,api
 from models import User
 app = Flask(__name__)
 
@@ -15,6 +15,27 @@ class Login(Resource):
 
         session['user_id'] = user.id
         return jsonify(user.to_dict())
+api.add_resource(Login,'/login')
+
+class CheckSession(Resource):
+
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return jsonify(user.to_dict())
+        else:
+            return jsonify({'message': '401: Not Authorized'}), 401
+
+api.add_resource(CheckSession, '/check_session')
+
+class Logout(Resource):
+    def get():
+     session['user_id'] = None
+     return jsonify({'message': '204: No Content'}), 204
+
+api.add_resource(Logout, '/logout')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
